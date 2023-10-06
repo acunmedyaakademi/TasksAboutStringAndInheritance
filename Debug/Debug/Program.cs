81021fd05aa0d8dbc5585a22908efaf3c41068cf
tugcetaskin <htugcetaskin@gmail.com>
2023-10-06 20:09:00 +0300

    Debug yüklendi

---
+++ Debug/Debug/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Android kullanan kullanıcı sayısını giriniz : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("iOS kullanan kullanıcı sayısını giriniz : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İos kullananların android kullananlara oranı : " + a / i);
+        }
+    }
+}
